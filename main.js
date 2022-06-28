e7ef7156d41e2698a534d9e843b0ad83f9930e27
Trung Hoang <trungtit12082001@gmail.com>
2022-06-28 18:46:48 +0700

    Reup

---
+++ main.js
@@ -0,0 +1,22 @@
+
+$ = document.querySelector.bind(document)
+$$ = document.querySelectorAll.bind(document)
+
+// Active Menu Item Selected
+const activeMenu = () => {
+    const menuItems = $$('.menu__item-hover')
+
+    menuItems.forEach(currentItem => {
+        currentItem.onclick = () => {
+            const activatedItem = $('.active')
+
+            if(activatedItem) {
+                activatedItem.classList.remove('active')
+            }
+            currentItem.classList.add('active')
+        }
+    })
+    
+}
+
+activeMenu()
